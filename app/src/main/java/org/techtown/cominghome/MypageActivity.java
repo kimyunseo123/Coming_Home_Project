0f090bfbd45d86bd36bad2f7008b49e3f432786b
leesj <leesj6368@naver.com>
2023-06-08 01:35:03 +0900

    1차완성

--- app/src/main/java/org/techtown/cominghome/MypageActivity.java
+++ app/src/main/java/org/techtown/cominghome/MypageActivity.java
@@ -2,47 +2,85 @@ package org.techtown.cominghome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 import android.os.Bundle;
 import android.view.View;
+import androidx.annotation.NonNull;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MypageActivity extends AppCompatActivity {
+    private Button buttonHome, buttonBoard, buttonQuiz, buttonMypage;
 
-    private static final String TAG = "MypageActivity";
-    private FirebaseAuth mAuth;
-    private FirebaseAuth mFirebaseAuth;
-    private TextView tv_result;
+    private static final String TAG = "MainActivity";
+    private static final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.profile_layout);
+        // bottom navigation buttons
+        buttonHome = findViewById(R.id.button_home);
+        buttonBoard = findViewById(R.id.button_board);
+        buttonQuiz = findViewById(R.id.button_quiz);
+        buttonMypage = findViewById(R.id.button_mypage);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
-        Intent intent = getIntent();
-        String nickName = intent.getStringExtra("nickName");
-
-        tv_result = findViewById(R.id.tv_result);
-        tv_result.setText("Nickname: " + nickName);
-
-        Button btn_logout = findViewById(R.id.btn_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
+        // set onClickListeners for bottom navigation buttons
+        buttonHome.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                mFirebaseAuth.signOut();
-
+            public void onClick(View v) {
                 Intent intent = new Intent(MypageActivity.this, AnimalActivity.class);
                 startActivity(intent);
-                finish(); // 앱끄기
+            }
+        });
+        buttonBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, CenterListActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonMypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MypageActivity.this, MypageActivity.class);
+                startActivity(intent);
             }
         });
 
-        //탈퇴 처리
-        //mFirebaseAuth.getCurrentUser().delete();
+        Button logout = (Button) findViewById(R.id.btn_logout2);
+        logout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button exit = (Button) findViewById(R.id.btn_exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
